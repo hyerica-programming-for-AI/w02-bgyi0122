52da53aede9718da9cee1282cf7bd2ee10a03907
bgyi0122 <bgyi0122@naver.com>
2026-03-05 15:31:52 +0900

    2주차 과제
    test.py
    test2.py

--- test.py
+++ test.py
@@ -1,3 +1,1 @@
 print("Hello world")
-
-a = 3
--- test2.py
+++ test2.py
@@ -1,3 +1,9 @@
 import torch
 import torch.nn as nn
-import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import dataloader
+
+trnasform = transforms.ToTensor()
+train_data = datasets.MNIST(root='./data', train=True, download=True, transform=trnasform)
+test_data = datasets.MNIST(root='./data', train=True, download=True, transform=trnasform)
