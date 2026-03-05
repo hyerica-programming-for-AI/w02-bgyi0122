60862c3815b862faac24362daf96637ef473516b
bgyi0122 <bgyi0122@naver.com>
2026-03-05 16:02:21 +0900

    question1, 2
    3주차 과제

---
+++ question_01.py
@@ -0,0 +1,10 @@
+def compute_circle_area(radius):
+    pi = 3.141592
+
+    area = None
+
+    area = radius * radius * pi
+
+    return area
+
+print(compute_circle_area(10))
---
+++ question_02.py
@@ -0,0 +1,10 @@
+def convert_to_string(number):
+    
+    stirng = None
+    string = str(number)
+
+    return string
+
+convert_to_string(123)
+
+    
